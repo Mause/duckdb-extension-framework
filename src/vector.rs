d20a80e7aa6cfde4b686377b5380217b41b65042
Elliana May <me@mause.me>
2022-10-20 05:29:05 +0000

    chore: improve doc strings

--- src/vector.rs
+++ src/vector.rs
@@ -54,9 +54,25 @@ impl Vector {
     pub fn get_column_type(&self) -> LogicalType {
         unsafe { LogicalType::from(duckdb_vector_get_column_type(self.0)) }
     }
+    /// Retrieves the validity mask pointer of the specified vector.
+    ///
+    /// If all values are valid, this function MIGHT return NULL!
+    ///
+    /// The validity mask is a bitset that signifies null-ness within the data chunk. It is a series of uint64_t values, where each uint64_t value contains validity for 64 tuples. The bit is set to 1 if the value is valid (i.e. not NULL) or 0 if the value is invalid (i.e. NULL).
+    ///
+    /// Validity of a specific value can be obtained like this:
+    ///
+    /// idx_t entry_idx = row_idx / 64; idx_t idx_in_entry = row_idx % 64; bool is_valid = validity_maskentry_idx & (1 << idx_in_entry);
+    ///
+    /// Alternatively, the (slower) row_is_valid function can be used.
+    ///
+    /// returns: The pointer to the validity mask, or NULL if no validity mask is present
     pub fn get_validity(&self) -> ValidityMask {
         unsafe { ValidityMask(duckdb_vector_get_validity(self.0), duckdb_vector_size()) }
     }
+    /// Ensures the validity mask is writable by allocating it.
+    ///
+    /// After this function is called, get_validity will ALWAYS return non-NULL. This allows null values to be written to the vector, regardless of whether a validity mask was present before.
     pub fn ensure_validity_writable(&self) {
         unsafe { duckdb_vector_ensure_validity_writable(self.0) };
     }
@@ -78,19 +94,30 @@ impl Debug for ValidityMask {
 }
 
 impl ValidityMask {
+    /// Returns whether or not a row is valid (i.e. not NULL) in the given validity mask.
+    ///
+    /// # Arguments
+    ///  * `row`: The row index
+    /// returns: true if the row is valid, false otherwise
     pub fn row_is_valid(&self, row: idx_t) -> bool {
         unsafe { duckdb_validity_row_is_valid(self.0, row) }
     }
+    /// In a validity mask, sets a specific row to either valid or invalid.
+    ///
+    /// Note that ensure_validity_writable should be called before calling get_validity, to ensure that there is a validity mask to write to.
+    ///
+    /// # Arguments
+    ///  * `row`: The row index
+    ///  * `valid`: Whether or not to set the row to valid, or invalid
     pub fn set_row_validity(&self, row: idx_t, valid: bool) {
         unsafe { duckdb_validity_set_row_validity(self.0, row, valid) }
     }
     /// In a validity mask, sets a specific row to invalid.
     ///
-    /// Equivalent to duckdb_validity_set_row_validity with valid set to false.
+    /// Equivalent to set_row_validity with valid set to false.
     ///
     /// # Arguments
-    /// validity: The validity mask
-    /// row: The row index
+    ///  * `row`: The row index
     pub fn set_row_invalid(&self, row: idx_t) {
         unsafe { duckdb_validity_set_row_invalid(self.0, row) }
     }
