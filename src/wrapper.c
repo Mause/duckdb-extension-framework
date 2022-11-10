3a478cb08fe6a5cddd12dda5994fe7a81ad43850
Elliana May <me@mause.me>
2022-11-10 14:59:08 +0800

    chore: implement duckdb_create_union

--- src/wrapper.c
+++ src/wrapper.c
@@ -1,6 +1,15 @@
 #include "wrapper.h"
 
-duckdb_logical_type duckdb_create_union() {
+duckdb_logical_type duckdb_create_union(int nmembers, const char** names, const duckdb_type* types) {
+  std::vector<std::pair<std::string, duckdb::LogicalType>> members;
+  for (idx_t i=0; i<nmembers; i++) {
+    members.emplace_back(
+      string(names[i]),
+      *(duckdb::LogicalType*) types[i]
+    );
+  }
+  
   duckdb::LogicalType* utype = new duckdb::LogicalType;
+  *utype = LogicalType::UNION(members);
   return utype;
 }
