cf328c0b82db9cfadffea693ebe8cc16ca27c9f8
Elliana May <me@mause.me>
2022-11-10 17:01:11 +0800

    chore: move c functions to cpp file

---
+++ src/wrapper.cpp
@@ -0,0 +1,28 @@
+#include "duckdb.hpp"
+#include "wrapper.h"
+
+#include <memory>
+
+static std::vector<std::pair<std::string, duckdb::LogicalType>>
+getVector(idx_t n_pairs, const char *const *names, duckdb_logical_type const *types) {
+    std::vector<std::pair<std::string, duckdb::LogicalType>> members;
+    for (idx_t i = 0; i < n_pairs; i++) {
+        members.emplace_back(
+                std::string(names[i]),
+                *(duckdb::LogicalType *) types[i]
+        );
+    }
+    return members;
+}
+
+duckdb_logical_type duckdb_create_struct_type(idx_t n_pairs, const char **names, const duckdb_logical_type *types) {
+    auto *stype = new duckdb::LogicalType;
+    *stype = duckdb::LogicalType::STRUCT(getVector(n_pairs, names, types));
+    return stype;
+}
+
+duckdb_logical_type duckdb_create_union(idx_t nmembers, const char **names, const duckdb_logical_type *types) {
+    auto *utype = new duckdb::LogicalType;
+    *utype = duckdb::LogicalType::UNION(getVector(nmembers, names, types));
+    return utype;
+}
